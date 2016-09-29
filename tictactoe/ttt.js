116a25a658789d8a5912eb6c157b670e880b7174
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-29 16:44:29 -0400

    tiic

--- tictactoe/ttt.js
+++ tictactoe/ttt.js
@@ -21,44 +21,64 @@ function startgame(){
     //rows//
     
     if (r1c1 + r1c2 + r1c3 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r1c1 + r1c2 + r1c3 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
     
     else if (r2c1 + r2c2 + r2c3 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r2c1 + r2c2 + r2c3 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
    
    else if (r3c1 + r3c2 + r3c3 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r3c1 + r3c2 + r3c3 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
    //cols//
    else if (r1c1 + r2c1 + r3c1 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r1c1 + r2c1 + r3c1 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
     
     else if (r1c2 + r2c2 + r3c2 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r1c2 + r2c2 + r3c2 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
   
    else if (r1c3 + r2c3 + r3c3 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r1c3 + r2c3 + r3c3 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
     // dia//
     else if (r1c1 + r2c2 + r3c3 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r1c1 + r2c2 + r3c3 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
      else if (r1c3 + r2c2 + r3c1 == 111){
-        alert(" X is the winner");}
+        alert(" X is the winner");
+    startOver();}
    else if (r1c3 + r2c2 + r3c1 == 555){
-            alert(" O is the winner");}
+            alert(" O is the winner");
+        startOver();}
 
             
          
     }
+
+function startOver() {
+    location.reload();
+};
