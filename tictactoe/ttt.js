9ab27505a4c1c8747d2c6a41f224a498e2ef9cda
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-29 21:29:38 -0400

    got cat working

--- tictactoe/ttt.js
+++ tictactoe/ttt.js
@@ -21,64 +21,88 @@ function startgame(){
     //rows//
     
     if (r1c1 + r1c2 + r1c3 == 111){
-        alert(" X is the winner");
-    startOver();}
+      setTimeout(xwin, 200);}
    else if (r1c1 + r1c2 + r1c3 == 555){
-            alert(" O is the winner");
-        startOver();}
+           setTimeout(owin, 200);}
     
     else if (r2c1 + r2c2 + r2c3 == 111){
-        alert(" X is the winner");
-    startOver();}
+        setTimeout(xwin, 200);}
    else if (r2c1 + r2c2 + r2c3 == 555){
-            alert(" O is the winner");
-        startOver();}
+           setTimeout(owin, 200);}
    
    else if (r3c1 + r3c2 + r3c3 == 111){
-        alert(" X is the winner");
-    startOver();}
+        setTimeout(xwin, 200);}
    else if (r3c1 + r3c2 + r3c3 == 555){
-            alert(" O is the winner");
-        startOver();}
+           setTimeout(owin, 200);}
    //cols//
    else if (r1c1 + r2c1 + r3c1 == 111){
-        alert(" X is the winner");
-    startOver();}
+        setTimeout(xwin, 200);}
    else if (r1c1 + r2c1 + r3c1 == 555){
-            alert(" O is the winner");
-        startOver();}
+          setTimeout(owin, 200);}
     
     else if (r1c2 + r2c2 + r3c2 == 111){
-        alert(" X is the winner");
-    startOver();}
+      setTimeout(xwin, 200);}
    else if (r1c2 + r2c2 + r3c2 == 555){
-            alert(" O is the winner");
-        startOver();}
+          setTimeout(owin, 200);}
   
    else if (r1c3 + r2c3 + r3c3 == 111){
-        alert(" X is the winner");
-    startOver();}
+        setTimeout(xwin, 200);}
    else if (r1c3 + r2c3 + r3c3 == 555){
-            alert(" O is the winner");
-        startOver();}
+            setTimeout(owin, 200);}
     // dia//
     else if (r1c1 + r2c2 + r3c3 == 111){
-        alert(" X is the winner");
-    startOver();}
+       setTimeout(xwin, 200);}
    else if (r1c1 + r2c2 + r3c3 == 555){
-            alert(" O is the winner");
-        startOver();}
+           setTimeout(owin, 200);}
      else if (r1c3 + r2c2 + r3c1 == 111){
-        alert(" X is the winner");
-    startOver();}
+        setTimeout(xwin, 200);}
    else if (r1c3 + r2c2 + r3c1 == 555){
-            alert(" O is the winner");
-        startOver();}
+            setTimeout(owin, 200);}
+ 
+else if (+r1c1 + +r1c2 + +r1c3 + +r2c1 + +r2c2 + +r2c3 + +r3c1 + +r3c2 + +r3c3 == 25){
+    alert(" The CAT wins");
+    startOver();}
+console.log(+r1c1 + +r1c2 + +r1c3 + +r2c1 + +r2c2 + +r2c3 + +r3c1 + +r3c2 + +r3c3 );
+}
 
-            
-         
-    }
+
+
 
 function startOver() {
-    location.reload();
+    $("#r1c1").val(0);
+    $("#r1c2").val(0);
+    $("#r1c3").val(0);
+    $("#r2c1").val(0);
+    $("#r2c2").val(0);
+    $("#r2c3").val(0);
+    $("#r3c1").val(0);
+    $("#r3c2").val(0);
+    $("#r3c3").val(0);
 };
+
+function xwin(){
+    alert(" X is the winner");
+    $("#xscore").empty(); 
+    xs();
+}
+function owin(){
+    alert(" O is the winner");
+    $("#oscore").empty();
+   os();
+}
+var xscr=0
+function xs() {
+    xscr++;
+     $("#xscore").append(xscr);
+     startOver();
+    };
+    
+    var oscr=0
+function os() {
+     oscr++;
+     $("#oscore").append(oscr);
+     startOver();
+    };
+    function restart() {
+    location.reload();}
+    
